23dec87b2071ac957697d5f9a597d8f6ffde58d9
Bruno Braga <bbraga@smithmicro.com>
2022-06-11 09:54:29 -0500

    first commit

---
+++ src/main/kotlin/com/bragdev/firstkotlinspringapplicationkotlintut/FirstKotlinSpringApplicationKotlinTutApplication.kt
@@ -0,0 +1,11 @@
+package com.bragdev.firstkotlinspringapplicationkotlintut
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstKotlinSpringApplicationKotlinTutApplication
+
+fun main(args: Array<String>) {
+    runApplication<FirstKotlinSpringApplicationKotlinTutApplication>(*args)
+}
---
+++ src/test/kotlin/com/bragdev/firstkotlinspringapplicationkotlintut/FirstKotlinSpringApplicationKotlinTutApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bragdev.firstkotlinspringapplicationkotlintut
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FirstKotlinSpringApplicationKotlinTutApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
