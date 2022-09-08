2108bdd9f2f6f7ecf04007ed6247c53aaba998fa
Bruno Braga <braga.bruno81@gmail.com>
2022-09-08 18:18:42 -0500

    second commit

--- src/main/kotlin/com/bragdev/firstkotlinspringapplicationkotlintut/FirstKotlinSpringApplicationKotlinTutApplication.kt
+++ src/main/kotlin/com/bragdev/firstkotlinspringapplicationkotlintut/FirstKotlinSpringApplicationKotlinTutApplication.kt
@@ -2,6 +2,15 @@ package com.bragdev.firstkotlinspringapplicationkotlintut
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class FirstKotlinSpringApplicationKotlinTutApplication
@@ -9,3 +18,32 @@ class FirstKotlinSpringApplicationKotlinTutApplication
 fun main(args: Array<String>) {
     runApplication<FirstKotlinSpringApplicationKotlinTutApplication>(*args)
 }
+
+@RestController
+class MessageResource(val service: MessageService) {
+    @GetMapping
+    fun index(): List<Message> = service.findMessages()
+
+    @PostMapping
+    fun post(@RequestBody message: Message) {
+        service.post(message)
+    }
+}
+@Service
+class MessageService(val db: MessageRepository) {
+
+    fun findMessages(): List<Message> = db.findMessages()
+
+    fun post(message: Message){
+        db.save(message)
+    }
+}
+
+interface MessageRepository : CrudRepository<Message, String> {
+
+    @Query("select * from messages")
+    fun findMessages(): List<Message>
+}
+
+@Table("MESSAGES")
+data class Message(@Id val id: String?, val text: String)
